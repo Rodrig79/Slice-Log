af6c9c78ca1b49b39c7c16dd28c608f47edad937
gwadsworth1224 <75765358+gwadsworth1224@users.noreply.github.com>
2020-12-10 15:19:33 -0800

    change hamburger
    streamlined hamburger menu operation

--- screens/home/components/hamburger.js
+++ screens/home/components/hamburger.js
@@ -1,79 +1,105 @@
 import React, {Component} from 'react';
-import {Animated, Dimensions, Text, View, TouchableOpacity} from 'react-native';
+import {Animated, Dimensions, Text, View, TouchableOpacity, Image} from 'react-native';
+import {getStatusBarHeight} from 'react-native-status-bar-height';
+
 
 class Hamburger extends Component {
 
-  render() {
-      var start;
-      var end;
-      if (this.props.status == 'start') {
-        start = -Dimensions.get('window').height/2.1;
-        end = -Dimensions.get('window').height/2.1
-      }
-      else if (this.props.status == 'open') {
-        start = -Dimensions.get('window').height/2.1;
-        end = -Dimensions.get('window').height/3.9
-      }
-      else {
-        start = -Dimensions.get('window').height/3.9
-        end = -Dimensions.get('window').height/2.1;
+    constructor() {
+      super()
+      this.state = {position: new Animated.Value(-Dimensions.get('window').height*0.125)}
+      this.end = Dimensions.get('window').height*0.1
+    }
 
-      }
-      var position = new Animated.Value(start);
-      Animated.timing(position, {
-        toValue: end,
+    startAnimation = () => {
+      Animated.timing(this.state.position, {
+        toValue: this.end,
         duration: 200,
         useNativeDriver: true,
-      }).start();
-      return(
-        <Animated.View style={[{width: '30%',
-                      height: '21%',
-                      position: 'absolute',
-                      left: 0,
-                      backgroundColor: '#07121B',
-                      zIndex: 3},
-                      {transform: [{translateY: position.interpolate({
-                        inputRange: [0, 100],
-                        outputRange: ['0%', '100%'],
-                      })}]},
-                    ]}>
-          <TouchableOpacity style={{width: '100%',
-                                    flex: 1,
-                                    borderLeft: 1,
+      }).start(() => {
+        if (this.end == Dimensions.get('window').height*0.1) {
+          this.end = -Dimensions.get('window').height*0.125
+          this.setState({position: new Animated.Value(Dimensions.get('window').height*0.1)})
+        }
+        else {
+          this.end = Dimensions.get('window').height*0.1
+          this.setState({position: new Animated.Value(-Dimensions.get('window').height*0.125)})
+        }
+      });
+    }
+
+    render() {
+      const transformStyle = {
+        transform : [{
+          translateY : this.state.position,
+        }]
+      }
+
+        return(
+          <TouchableOpacity style={{width: '15%',
+                                    height: '50%',
                                     backgroundColor: '#07121B',
+                                    marginTop: getStatusBarHeight(),
                                     alignItems: 'center',
                                     justifyContent: 'center',
-                                    borderWidth: 3,
-                                    borderColor: '#000000'}}
-                            onPress={() => {this.props.clearPies()}}>
-            <Text style={{color: '#AAAAAA', fontSize: 20}}>SUBMIT PIES</Text>
+                                    zIndex: 5}}
+                            onPress={() => {}}>
+            <Image source={require('../../../assets/hamburgerIcon.png')}
+                   style={{width:'70%', height: '70%', marginLeft: '40%', resizeMode: 'contain'}}>
+            </Image>
+            <Animated.View style={[{width: '30%',
+                          height: '225%',
+                          position: 'absolute',
+                          left: 0,
+                          backgroundColor: '#07121B',
+                          zIndex: 3},
+                          transformStyle
+                        ]}>
+              <TouchableOpacity style={{width: '100%',
+                                        flex: 1,
+                                        borderLeft: 1,
+                                        backgroundColor: '#07121B',
+                                        alignItems: 'center',
+                                        justifyContent: 'center',
+                                        borderWidth: 3,
+                                        borderColor: '#000000'}}
+                                onPress={() => {this.props.openDailyPrep()
+                                                this.startAnimation()}}>
+                <Text style={{color: '#AAAAAA', fontSize: 20}}
+                      adjustsFontSizeToFit
+                      numberOfLines={1}>DAILY PREP</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={{width: '100%',
+                                        flex: 1,
+                                        borderLeft: 1,
+                                        backgroundColor: '#07121B',
+                                        alignItems: 'center',
+                                        justifyContent: 'center',
+                                        borderWidth: 3,
+                                        borderColor: '#000000',
+                                        borderTopWidth: 0}}
+                                onPress={() => {this.props.openInventory()
+                                                this.startAnimation()}}>
+                <Text style={{color: '#AAAAAA', fontSize: 20}}
+                      adjustsFontSizeToFit
+                      numberOfLines={1}>INVENTORY</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={{width: '100%',
+                                        flex: 1,
+                                        borderLeft: 1,
+                                        backgroundColor: '#07121B',
+                                        alignItems: 'center',
+                                        justifyContent: 'center'}}
+                                onPress={() => {this.props.openReports()
+                                                this.startAnimation()}}>
+                <Text style={{color: '#AAAAAA', fontSize: 20}}
+                      adjustsFontSizeToFit
+                      numberOfLines={1}>REPORTS</Text>
+              </TouchableOpacity>
+            </Animated.View>
           </TouchableOpacity>
-          <TouchableOpacity style={{width: '100%',
-                                    flex: 1,
-                                    borderLeft: 1,
-                                    backgroundColor: '#07121B',
-                                    alignItems: 'center',
-                                    justifyContent: 'center',
-                                    borderWidth: 3,
-                                    borderColor: '#000000',
-                                    borderTopWidth: 0}}
-                            onPress={() => {this.props.openTempLog()
-                                            this.props.updateHamburger()}}>
-            <Text style={{color: '#AAAAAA', fontSize: 20}}>TEMP LOG</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={{width: '100%',
-                                    flex: 1,
-                                    borderLeft: 1,
-                                    backgroundColor: '#07121B',
-                                    alignItems: 'center',
-                                    justifyContent: 'center'}}
-                            onPress={() => {this.props.openMenu()
-                                            this.props.updateHamburger()}}>
-            <Text style={{color: '#AAAAAA', fontSize: 20}}>MENU</Text>
-          </TouchableOpacity>
-        </Animated.View>
-      );
-  }
+        );
+    }
 }
 
 export default Hamburger
