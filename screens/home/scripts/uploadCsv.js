1264cd1862734376d600ad45a6767db53abafd00
gwadsworth1224 <75765358+gwadsworth1224@users.noreply.github.com>
2020-12-22 19:02:19 -0800

    upload changes

--- screens/home/scripts/uploadCsv.js
+++ screens/home/scripts/uploadCsv.js
@@ -1,5 +1,6 @@
-import * as Google from 'expo-google-app-auth';
-import * as FileSystem from 'expo-file-system';
+import * as Google from 'expo-google-app-auth'
+import * as AppAuth from 'expo-app-auth'
+import * as FileSystem from 'expo-file-system'
 
 export async function uploadCsv(date, uri, csv) {
   var name = `"Slice-Log-` + `${date}"`
@@ -10,18 +11,26 @@ export async function uploadCsv(date, uri, csv) {
         "1SdRy6zEnkp1a8Y2J3_1q1w4JbgczMlqu"
       ]
   }
-  const { type, accessToken, user } = await Google.logInAsync({
-    iosClientId: `457820335662-9emkpcnjk952sqr7dq3io1923ivcbqp6.apps.googleusercontent.com`,
-    androidClientId: `457820335662-kid2mvk1travh8f50v3p8je6g226hmfi.apps.googleusercontent.com`,
-    iosStandaloneAppClientId: `457820335662-9emkpcnjk952sqr7dq3io1923ivcbqp6.apps.googleusercontent.com`,
-    androidStandaloneAppClientId: `457820335662-iakle1qk9tk85icoa5vqadjmbj3s8180.apps.googleusercontent.com`,
-    webClientId: `457820335662-qtvgg1216he2keknrb142t1k5m58jtbg.apps.googleusercontent.com`,
+  // const { type, accessToken, user } = await Google.logInAsync({
+  //   iosClientId: `457820335662-9emkpcnjk952sqr7dq3io1923ivcbqp6.apps.googleusercontent.com`,
+  //   androidClientId: `457820335662-kid2mvk1travh8f50v3p8je6g226hmfi.apps.googleusercontent.com`,
+  //   iosStandaloneAppClientId: `457820335662-9emkpcnjk952sqr7dq3io1923ivcbqp6.apps.googleusercontent.com`,
+  //   androidStandaloneAppClientId: `457820335662-iakle1qk9tk85icoa5vqadjmbj3s8180.apps.googleusercontent.com`,
+  //   webClientId: `457820335662-qtvgg1216he2keknrb142t1k5m58jtbg.apps.googleusercontent.com`,
+  //   scopes: ['https://www.googleapis.com/auth/drive'],
+  //   behavior: 'web',
+  //   redirectUrl: `${AppAuth.OAuthRedirect}:/oauthredirect`
+  // });
+  // if (type === 'success') {
+  //   console.log(type);
+  // }
+
+  var user = await AppAuth.authAsync({
+    issuer: 'https://accounts.google.com',
     scopes: ['https://www.googleapis.com/auth/drive'],
-    behavior: 'web'
-  });
-  if (type === 'success') {
-    console.log(type);
-  }
+    clientId: '457820335662-kid2mvk1travh8f50v3p8je6g226hmfi.apps.googleusercontent.com',
+  })
+
 
   var boundary = "--boundary1234"
   var body = ""
@@ -42,7 +51,7 @@ export async function uploadCsv(date, uri, csv) {
   fetch('https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart', {
     method: 'POST',
     headers: {
-      Authorization: 'Bearer ' + accessToken,
+      Authorization: 'Bearer ' + user.accessToken,
       "Content-Type": "multipart/related; boundary=boundary1234",
       "Content-Size": (await FileSystem.getInfoAsync(uri)).size.toString()
     },
